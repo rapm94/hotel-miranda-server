158bf553cf683cb7231abb3f3aae43adb28e7613
Raúl Puigbó <raulapuigbo@gmail.com>
2022-01-18 10:27:10 +0100

    Compiled TypeScript

--- auth/auth.js
+++ auth/auth.js
@@ -13,30 +13,7 @@ const localStrategy = require('passport-local').Strategy;
 const User = require('../models/user.schema');
 const JWTStrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
-//const { PrismaClient} = require('@prisma/client')
 const bcrypt = require('bcrypt');
-//const prisma = new PrismaClient()
-/*  passport.use(
-  'signup',
-  new localStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-    async (email: string, password: string, done: any) => {
-      try {
-        const user = await User.findOne({ email: email })
-        if (user) {
-          return done(null, false, { message: 'User already exists' })
-        }
-        const newUser = await User.create({ email, password })
-        return done(null, newUser)
-      } catch (error) {
-        return done(error)
-      }
-    },
-  ),
-) */
 passport.use('login', new localStrategy({
     usernameField: 'email',
     passwordField: 'password',
@@ -71,28 +48,3 @@ passport.use(new JWTStrategy({
         return done(error);
     }
 })));
-/* passport.use(
-  new JWTStrategy(
-    {
-      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
-    },
-    async (jwtPayload: any, done: any) => {
-      try {
-        const user = await prisma.user.findUnique({
-          where: {
-            email: jwtPayload.uid.email,
-          },
-        })
-        if (!user) {
-          return done(null, false)
-        }
-        return done(null, user)
-      } catch (error) {
-        return done(error)
-      }finally{
-        prisma.$disconnect()
-      }
-    },
-  ),
-) */
